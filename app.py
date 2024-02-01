44288218d40dd1282407a56b9f91a5f7bcf63a04
dhaval bhatt <bdhaval689@gmail.com>
2024-02-01 13:26:08 +0500

    prompt updated

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import cohere
 from cohere.responses.chat import StreamEvent
 import streamlit as st
 import uuid
+from datetime import date
 
 co = cohere.Client(getenv("CO_API"))
 # Use CSS to add custom styles to your UI
@@ -15,21 +16,21 @@ with open("style.css") as f:
 # Side bar
 if st.sidebar.button("New Chat", key="clear_chat"):
     st.session_state.messages = []
-st.sidebar.title("AVA")
-st.sidebar.markdown("It is your AI companion for Quality Assurance (QA)")
-st.sidebar.markdown("It helps you debug the code")
-st.sidebar.markdown("Provide your code or ask for help")
+st.sidebar.title("SEO AI Companion")
+st.sidebar.markdown("Analyse latest keywords trend")
+st.sidebar.markdown("Write blogs, plan new content")
 
 # Main content
-st.title("AI Companion")
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
+st.title("SEO AI Companion")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if "unique_id" not in st.session_state:
     st.session_state.unique_id = str(uuid.uuid4())
+
+
+if "preamble" not in st.session_state:
+    st.session_state.preamble = f"""You are an expert SEO Specialist with 30 Years of experience in marketing. Your task is to use internet and offer a deep-dive consultation tailored to the client's issue. Ensure the user feels understood, guided, and satisfied with your expertise. The consultation is deemed successful when the user explicitly communicates their contentment with the solution. Today's date is {date.today().strftime('%A, %B %d, %Y')}."""
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -43,14 +44,14 @@ if prompt := st.chat_input("Ask a question"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        with st.spinner("✨🤖✨"):
+        with st.spinner("🤖"):
             resp = co.chat(
                 model="command-light-nightly",
                 message=prompt,
                 temperature=0.7,
                 conversation_id=st.session_state.unique_id,
                 # return_preamble=True,
-                # preamble_override=preamble,
+                preamble_override=st.session_state.preamble,
                 prompt_truncation="AUTO",
                 citation_quality="accurate",
                 connectors=[{"id": "web-search"}],
